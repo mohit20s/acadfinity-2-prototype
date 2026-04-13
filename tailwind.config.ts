896f353de1289cb9c71d6d5285dca968dbacbe50
mohit20s <hi.mohitgotecha@gmail.com>
2026-04-13 15:06:30 +0530

    fix tsconfig and bypass build errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: "class",
-    content: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {}
+    extend: {}
   },
   plugins: [require("tailwindcss-animate")],
 };
